09a4cff69429967b09c05b697055ca6c30d09aef
Soufiane Salouf <soufiane.salouf@ibm.com>
2022-07-21 21:33:40 +0100

    Init project

---
+++ YassirTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YassirTest
+//
+//  Created by Soufiane Salouf on 21/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
